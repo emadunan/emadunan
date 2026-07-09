f2aa8ae02f3eee7ffd906bb90d5dcf32e2db9755
emadunan <emadunan@gmail.com>
2026-07-09 15:34:34 +0300

    feat: Make the Appheader logo in the center in small screens with big size

--- packages/react-ui/src/layout/index.ts
+++ packages/react-ui/src/layout/index.ts
@@ -4,4 +4,3 @@ export { default as Header } from "./Header";
 export { default as AppHeader } from "./AppHeader";
 export { default as Logo } from "./Logo";
 export { default as Main } from "./Main";
-export { default as SmartHeader } from "./SmartHeader";
